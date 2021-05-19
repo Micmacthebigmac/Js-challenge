73c5ce4224fd41c8b1706cab463a5ae6b142fe29
Hervé <h.richard.codeur.onlinem>
2021-05-19 11:32:29 +0200

    progress-bar

---
+++ progress-bar/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function () {
+    let a = document.documentElement.scrollTop;
+    console.log('a=', a);
+    let b = document.documentElement.scrollHeight;
+    console.log('b=', b);
+
+    let c = document.documentElement.clientHeight;
+    console.log('c=', c);
+
+    let d = b - c;
+    console.log(d);
+    let e = (a / d) * 100;
+    console.log(e);
+
+    document.getElementById('bar').style.width =e + '%';
+})
