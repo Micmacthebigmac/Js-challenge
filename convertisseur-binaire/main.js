9e0fdce74502980183d6431d57acc6ebb3deb193
Hervé <h.richard.codeur.onlinem>
2021-07-16 11:19:20 +0200

    convert work

--- convertisseur-binaire/main.js
+++ convertisseur-binaire/main.js
@@ -1,5 +1,14 @@
 const convert = document.getElementById('convert') ;
+let result = document.getElementById('convert') ;
+let initial = 0 ;
 
 convert.addEventListener('click', function() {
-
+    let stringConverted = [];
+    let stringForm = document.getElementById('msgToCode').value ;
+    for (let index = 0; index < stringForm.length; index++) {
+        stringConverted = stringForm[index].charCodeAt(0).toString(2);
+        console.log(stringConverted);
+    }
+/*     let stringFinish = stringConverted.join();
+    console.log(stringFinish); */
 });
