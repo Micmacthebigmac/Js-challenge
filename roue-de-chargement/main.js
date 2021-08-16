e8d090752e2dd3bcb8251ea9740a9070cd822d96
Hervé <h.richard.codeur.onlinem>
2021-08-16 09:14:01 +0200

    add roue-de-chargement

---
+++ roue-de-chargement/main.js
@@ -0,0 +1,8 @@
+const loading = document.getElementById("loadingAnimation") ;
+const content = document.getElementById("displayContent") ;
+
+window.onload = // écrivez du code ici 
+
+function showContent() {
+  // et là
+}
