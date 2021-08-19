30a55ca93e1a852fee7c37221eee5a58b8dd75ed
Hervé <h.richard.codeur.onlinem>
2021-08-19 09:45:34 +0200

    function js work with screen.availHeight & Width

--- phone-rotate-animation/main.js
+++ phone-rotate-animation/main.js
@@ -6,14 +6,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
   
   function detectOrientation(){
 
-    if (orientation === "landscape-primary") {
-      console.log("That looks good.");
-    } else if (orientation === "landscape-secondary") {
-      console.log("Mmmh... the screen is upside down!");
-    } else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
-      console.log("Mmmh... you should rotate your device to landscape");
-    } else if (orientation === undefined) {
-      console.log("The orientation API isn't supported in this browser :(");
+    if(screen.availHeight > screen.availWidth){
+      console.log("LANDSCAPE.");
+      document.getElementById('rotateScreen').style.display='block';
+      document.getElementById('displayContent').style.display='block';
+    } else if(screen.availHeight < screen.availWidth){
+        console.log("PORTRAIT.");
+        document.getElementById('rotateScreen').style.display='none';
+        document.getElementById('displayContent').style.display='none';
     }
   }
-  
