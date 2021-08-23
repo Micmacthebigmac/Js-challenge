19761a56d2bdad165cd618e4007182e25db73fda
Hervé <h.richard.codeur.onlinem>
2021-08-23 11:06:48 +0200

    if try & failed

--- logo-agrandissent-retrecissent/main.js
+++ logo-agrandissent-retrecissent/main.js
@@ -1,12 +1,15 @@
 const logo = document.getElementById('logo');
 let value = 0 ;
 function animLogo(){
-     for ( value= 0; value= 240; value++) {
-         logo.style.height= 'calc(0 + 50px)';
-         logo.style.width= 'calc(0 + 50px)';
-         logo.style.fontSize= 'calc(20px + 50px)';
-         console.log(value);
-     }
+
+if (value > 240) {
+    value++;
+    logo.style.height= 'calc(0 + 50px)';
+    logo.style.width= 'calc(0 + 50px)';
+    logo.style.fontSize= 'calc(20px + 50px)';
+    console.log(value);
+}
+
     }
 
 setInterval(animLogo, 10);
